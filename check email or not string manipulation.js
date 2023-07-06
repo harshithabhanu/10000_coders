5efcf9d5eef21819e2f15a3bbbd9574f1f227884
harshithabhanu <129141932+harshithabhanu@users.noreply.github.com>
2023-07-06 12:43:10 +0530

    Add files via upload

---
+++ check email or not string manipulation.js
@@ -0,0 +1,14 @@
+//Program To Create a function to check email or not string manipulation
+
+function mail()
+{
+    let a= prompt("Enter your mail");
+    let b=a.endsWith("@gmail.com");
+    if(b){
+    console.log("valid email");
+}
+else{
+    console.log("b");
+}
+}
+mail();
